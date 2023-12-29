1372c41d3557ca66548ffc55b177044d987d27b7
Davi <cdavic13@gmail.com>
2023-12-28 23:25:20 -0300

    deixando mais bunito

--- resources/views/lista-impressoes.blade.php
+++ resources/views/lista-impressoes.blade.php
@@ -1,7 +1,31 @@
-<div>
-    <h1>OI MAIOR</h1> 
-    <h6>oi menor</h6>
+
+<head>
+    <style>
+
+        body {
+            background-color: rgb(25, 30, 34);
+        }
+
+        h1 {
+            color: rgb(205, 205, 205);
+            margin-left: 40px;
+        }
+        h4,p {
+            color: rgb(205, 205, 205);
+            margin-left: 20px;
+        }
+
+</style>
+</head>
+
+<body>
+
+    <h1>Lista de impressões</h1>
+    <p>Lista:</p>
+
     @foreach ($impressoes as $impressao)
-     <h4> {{$impressao}} </h4>
+        <h4> {{$impressao}} </h4>
     @endforeach
-</div>
+
+</body>
+
